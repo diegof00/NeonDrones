6d317fc26dab767e1d70145f0a04177e069c4ad9
Diego <dfreyes00@gmail.com>
2025-11-08 22:31:59 -0500

    disabled autofire

--- core/src/main/java/com/abyasoft/neondrones/gameplay/Controls.java
+++ core/src/main/java/com/abyasoft/neondrones/gameplay/Controls.java
@@ -4,13 +4,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 
 public class Controls {
-    public boolean left, right, up, down, fire;
+    public boolean left, right, up, down, fire, bomb;
 
     public void poll() {
         left  = Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT);
         right = Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT);
         up    = Gdx.input.isKeyPressed(Input.Keys.W) || Gdx.input.isKeyPressed(Input.Keys.UP);
         down  = Gdx.input.isKeyPressed(Input.Keys.S) || Gdx.input.isKeyPressed(Input.Keys.DOWN);
-        fire  = true; // autofire en el prototipo (si prefieres tecla: Input.Keys.SPACE)
+        fire  = Gdx.input.isKeyPressed(Input.Keys.SPACE); // autofire en el prototipo (si prefieres tecla: Input.Keys.SPACE)a
+        bomb  = Gdx.input.isKeyPressed(Input.Keys.E);
     }
 }
